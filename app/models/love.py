0898cd4da9eeaf1ecc1980533e76662d10d9d060
gem install redis-dump <307440205@qq.com>
2020-02-21 22:08:13 +0800

    更改数据名称

--- app/models/love.py
+++ app/models/love.py
@@ -6,6 +6,8 @@
 @Email   : tianjincn@163.com
 @Software: PyCharm
 """
+from datetime import datetime
+
 from lin.interface import InfoCrud as Base
 
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, DateTime, Boolean
@@ -14,40 +16,40 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Float, DateTime, Boo
 class Love_user(Base):
 
     __tablename__ = "love_user"
-    Id = Column(Integer, primary_key=True, autoincrement=True, comment="用户id")
-    Username = Column(String(32), comment="用户名称")
-    Openid = Column(String(62), comment="用户标识")
-    Unionid = Column(String(62), comment="接口凭证")
-    Portraits = Column(String(62), comment="用户头像")
-    Sex = Column(String(120), comment="用户性别")
-    Address = Column(String(4), comment="所在城市")
-    Language = Column(String(15), comment="语言")
-    Subscribe_tiem = Column(String(20), comment="关注时间")
-    Province = Column(String(20), comment="所在的省份")
+    Id = Column("id", Integer, primary_key=True, autoincrement=True, comment="用户id")
+    Username = Column("username", String(32), comment="用户名称")
+    Openid = Column("openid", String(62), comment="用户标识")
+    Unionid = Column("unionid", String(62), comment="接口凭证")
+    Portraits = Column("portraits", String(62), comment="用户头像")
+    Sex = Column("sex", String(120), comment="用户性别")
+    Address = Column("address", String(4), comment="所在城市")
+    Language = Column("language", String(15), comment="语言")
+    Subscribe_tiem = Column("subscribe", DateTime, default=datetime.now, comment="关注时间")
+    Province = Column("province", String(20), comment="所在的省份")
 
 
 class Love_message(Base):
     __tablename__ = "love_message"
-    Id = Column(Integer, primary_key=True, autoincrement=True, comment="信息id")
-    Uid = Column(Integer, ForeignKey("love_user.Id"), comment="用户id")
-    Phone = Column(Integer, nullable=False, comment="电话号码")
-    Cardid = Column(String(28), nullable=False, comment="身份证号码")
-    Census = Column(String(62), nullable=False, comment="户籍所在地")
-    Images = Column(String(62), nullable=False, comment="用户图片")
-    Hobby = Column(String(120), nullable=False, comment="兴趣爱好")
-    Stature = Column(String(4), nullable=False, comment="身高")
-    Weight = Column(String(4), nullable=False, comment="体重")
-    Blood = Column(String(6), nullable=False, comment="血型")
-    Nation = Column(String(12), nullable=False, comment="民族")
-    Standards = Column(String(200), nullable=False, comment="择偶标准")
-    Education = Column(String(20), nullable=False, comment="学历")
-    Vehicle = Column(String(3), nullable=False, comment="是否有车")
-    Monthly = Column(Float, nullable=False, comment="月薪")
-    Profession = Column(String(12), nullable=False, comment="职业性质")
-    Marriage = Column(String(20), nullable=False, comment="婚恋情况")
-    Housing = Column(String(62), nullable=False, comment="住房情况")
-    Personage = Column(String(500), nullable=False, comment="个人介绍")
-    Logindate = Column(Integer, nullable=False, comment="登录次数")
+    Id = Column("id", Integer, primary_key=True, autoincrement=True, comment="信息id")
+    Uid = Column("uid", Integer, ForeignKey("love_user.Id"), comment="用户id")
+    Phone = Column("phone", Integer, nullable=False, comment="电话号码")
+    Cardid = Column("cardid", String(28), nullable=False, comment="身份证号码")
+    Census = Column("census", String(62), nullable=False, comment="户籍所在地")
+    Images = Column("images", String(62), nullable=False, comment="用户图片")
+    Hobby = Column("hobby", String(120), nullable=False, comment="兴趣爱好")
+    Stature = Column("stature", String(4), nullable=False, comment="身高")
+    Weight = Column("weight", String(4), nullable=False, comment="体重")
+    Blood = Column("blood", String(6), nullable=False, comment="血型")
+    Nation = Column("nation", String(12), nullable=False, comment="民族")
+    Standards = Column("standards", String(200), nullable=False, comment="择偶标准")
+    Education = Column("education", String(20), nullable=False, comment="学历")
+    Vehicle = Column("vehicle", String(3), nullable=False, comment="是否有车")
+    Monthly = Column("monthly", Float, nullable=False, comment="月薪")
+    Profession = Column("profession", String(12), nullable=False, comment="职业性质")
+    Marriage = Column("marriage", String(20), nullable=False, comment="婚恋情况")
+    Housing = Column("housing", String(62), nullable=False, comment="住房情况")
+    Personage = Column("presonage", String(500), nullable=False, comment="个人介绍")
+    Logindate = Column("logindate", Integer, nullable=False, comment="登录次数")
 
 
 class Love_payment(Base):
